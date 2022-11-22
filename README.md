415e58ea209207e61e477aafdf6ff47c45b9794d
gaozijie <1906497996@qq.com>
2022-11-22 17:23:15 +0800

    新增公约数和公倍数

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 + `DateUtil`：LocalDateTime转Date、Date转LocalDateTime
 + `HttpUtil`：获取IP地址
 + `LocationUtil`：根据IP地址获取用户位置、经纬度计算距离
++ `MathUtil`：求最大公约数、求最小公倍数
---
+++ src/main/java/com/letere/util/MathUtil.java
@@ -0,0 +1,39 @@
+package com.letere.util;
+
+public class MathUtil {
+
+    /**
+     * Greatest Common Divisor 最大公约数 <p>
+     * 【辗转相除法】
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int getGreatestCommonDivisor(int a, int b) {
+        // 保证a最大
+        int temp;
+        if (b > a) {
+            temp = b;
+            b = a;
+            a= temp;
+        }
+        // 存在余数，继续求余数的余数
+        while ((temp=a%b) != 0) {
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+
+    /**
+     * Least Common Multiple 最小公倍数 <p>
+     * [(a * b) / ab的最大公约数]
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int getLeastCommonMultiple(int a, int b) {
+        int gcd = getGreatestCommonDivisor(a, b);
+        return a * b / gcd;
+    }
+}
