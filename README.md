3aa99bb56b6e9099ac2db2a6b64f039a366d168a
gaozijie <1906497996@qq.com>
2023-09-27 15:18:48 +0800

    移除实体类

--- README.md
+++ README.md
@@ -10,5 +10,5 @@
 + `DateUtil`：LocalDateTime转Date、Date转LocalDateTime、获取当天/周/月的起始时间
 + `HttpUtil`：获取IP地址
 + `LocationUtil`：根据IP地址获取用户位置、经纬度计算距离
-+ `MathUtil`：求最大公约数、求最小公倍数
++ `MathUtil`：求最大公约数、求最小公倍数、求数据数组之间百分占比
 + `CmdUtil`：执行系统命令
--- src/main/java/com/letere/util/MathUtil.java
+++ src/main/java/com/letere/util/MathUtil.java
@@ -1,5 +1,10 @@
 package com.letere.util;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MathUtil {
 
     /**
@@ -35,5 +40,71 @@ public class MathUtil {
     public static int getLeastCommonMultiple(int a, int b) {
         int gcd = getGreatestCommonDivisor(a, b);
         return a * b / gcd;
+    }
+
+    /**
+     * 计算数据占比
+     * @param nums 数据大小数组(Double类型)
+     * @return 数据占比数组
+     */
+    public static List<BigDecimal> calRateByDouble(List<Double> nums) {
+        if (nums == null || nums.size() == 0) {
+            return new ArrayList<>();
+        }
+        // 转成decimal数据
+        BigDecimal totalNum = BigDecimal.ZERO;
+        List<BigDecimal> decimalNums = new ArrayList<>(nums.size());
+        BigDecimal decimalNum;
+        for (Double num : nums) {
+            decimalNum = BigDecimal.valueOf(num);
+            totalNum = totalNum.add(decimalNum);
+            decimalNums.add(decimalNum);
+        }
+        // 计算占比
+        return calRate(decimalNums, totalNum);
+    }
+
+    /**
+     * 计算数据占比
+     * @param nums 数据大小数组(Long类型)
+     * @return 数据占比数组
+     */
+    public static List<BigDecimal> calRateByLong(List<Long> nums) {
+        if (nums == null || nums.size() == 0) {
+            return new ArrayList<>();
+        }
+        // 转成decimal数据
+        BigDecimal totalNum = BigDecimal.ZERO;
+        List<BigDecimal> decimalNums = new ArrayList<>(nums.size());
+        BigDecimal decimalNum;
+        for (Long num : nums) {
+            decimalNum = BigDecimal.valueOf(num);
+            totalNum = totalNum.add(decimalNum);
+            decimalNums.add(decimalNum);
+        }
+        // 计算占比
+        return calRate(decimalNums, totalNum);
+    }
+
+    /**
+     * 计算数据占比
+     * @param nums 数据大小数组
+     * @param totalNum 总数据大小
+     * @return 数据占比数组
+     */
+    private static List<BigDecimal> calRate(List<BigDecimal> nums, BigDecimal totalNum) {
+        if (nums == null || nums.size() == 0) {
+            return new ArrayList<>();
+        }
+        List<BigDecimal> rates = new ArrayList<>(16);
+        BigDecimal totalRate = BigDecimal.ONE;
+        BigDecimal rate;
+        for (int i=0; i<nums.size()-1; i++) {
+            rate = nums.get(i).divide(totalNum, 4, RoundingMode.DOWN);
+            totalRate = totalRate.subtract(rate);
+            rates.add(totalRate);
+        }
+        rates.add(totalRate);
+        return rates;
     }
 }
