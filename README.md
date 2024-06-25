6080fa8c735735a44d4b401bc25933e5c66abf48
gaozijie <1906497996@qq.com>
2024-06-25 09:28:33 +0800

    新增雪花算法工具类

--- README.md
+++ README.md
@@ -6,9 +6,10 @@
 
 ### 工具类介绍
 
++ `CmdUtil`：执行系统命令
 + `CodeUtil`：获取数字随机码，SHA字符加密
 + `DateUtil`：LocalDateTime转Date、Date转LocalDateTime、获取当天/周/月的起始时间
 + `HideUtil`：加密工具，加密手机号，邮箱信息
 + `HttpUtil`：获取IP地址
 + `MathUtil`：求最大公约数、求最小公倍数、求数据数组之间百分占比
-+ `CmdUtil`：执行系统命令
++ `SnowflakeUtil`：雪花算法生成id
---
+++ src/main/java/com/letere/util/SnowflakeUtil.java
@@ -0,0 +1,77 @@
+package com.letere.util;
+
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+
+/**
+ * 雪花算法工具类
+ * @author gaozijie
+ * @since 2024-06-24
+ */
+public class SnowflakeUtil {
+
+    /**
+     * 起始时间戳（暂定：2024-01-01 00:00:00，按实际情况修改，最大使用年长69年）
+     */
+    private final static long START_TIMESTAMP = LocalDateTime
+            .of(2024, 1, 1, 0, 0, 0)
+            .toInstant(ZoneOffset.ofTotalSeconds(TimeZone.getDefault().getRawOffset() / 1000))
+            .toEpochMilli();
+
+    /**
+     * 序列号掩码
+     */
+    private final static long SEQ_MASK = ~(-1L << 12);
+
+    /**
+     * 机器码（10位，按实际情况修改，不同服务要用不同的机器码区分）
+     */
+    private final static long INSTANCE = 1L;
+
+    /**
+     * 时间戳（41位）
+     */
+    private static long timestamp = -1L;
+
+    /**
+     * 序列号（12位），并发生成用
+     */
+    private static long seq = 0L;
+
+    /**
+     * 生成id
+     * @return 雪花算法Id
+     */
+    public synchronized static long getId(){
+        long now = System.currentTimeMillis();
+        // 当前时间<记录时间（时钟回拨）
+        if (now < timestamp){
+            throw new RuntimeException("时钟回拨异常，请将时间调整正常后再进行获取");
+        }
+        // 当前时间=记录时间，序列号+1，并做溢出处理
+        if (now == timestamp){
+            seq = (seq + 1) & SEQ_MASK;
+            // 溢出则等待1ms，并当前时间+1ms，再重新获取当前时间戳
+            if (seq == 0L){
+                try {
+                    Thread.sleep(1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                now = System.currentTimeMillis();
+            }
+        } else {
+            seq = 0L;
+        }
+        //记录当前时间
+        timestamp = now;
+        // 组合雪花算法id long(64) =（符号位(1)-时间戳差值(41)-机器码(10)-序列号(12)）
+        // 时间戳差值左移（64-1-41=22）位
+        long handleTimeStamp = (now - START_TIMESTAMP) << 22;
+        // 机器码左移（22-10=12）位
+        long handleInstance = INSTANCE << 12;
+        return handleTimeStamp | handleInstance | seq;
+    }
+}
